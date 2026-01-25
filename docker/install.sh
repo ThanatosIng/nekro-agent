384315bf2be841092d190ffa2356f11c2fadc58a
SevenNine <68588725+SevenNine233@users.noreply.github.com>
2026-01-25 22:47:55 +0800

    Update install.sh

--- docker/install.sh
+++ docker/install.sh
@@ -383,7 +383,7 @@ fi
 
 # 拉取沙盒镜像
 echo "拉取沙盒镜像..."
-if ! sudo docker pull kromiose/nekro-agent-sandbox; then
+if ! sudo docker pull kromiose/nekro-agent-sandbox:preview; then
     echo "Error: 无法拉取沙盒镜像，请检查您的网络连接。"
     exit 1
 fi
